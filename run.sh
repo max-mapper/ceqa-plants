c35dc08ac52a0ea70dc3343c77eb2720b2de8c13
maxogden <max@maxogden.com>
2024-06-29 18:27:53 -0700

    fix path

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ todaydir="ceqa-plants/docs/output/$(date --date yesterday "+%Y/%m/%d")"
 mkdir -p $todaydir
 find zips -iname '*.pdf' -exec sh -c 'pdftotext "{}" - | grep --color --with-filename --label="{}" -f rare.txt' \; > "$todaydir/matches.txt"
 node html.js "$todaydir/matches.txt" > "$todaydir/index.html"
-tree docs/output -T "CEQA Rare Plant Species Mentions" -H https://max-mapper.github.io/ceqa-plants/output -o ceqa-plants/docs/index.html
 cd ceqa-plants
+tree docs/output -T "CEQA Rare Plant Species Mentions" -H https://max-mapper.github.io/ceqa-plants/output -o docs/index.html
 git config credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
 git config --global user.email "maxosmail@gmail.com"
 git config --global user.name "Max"
